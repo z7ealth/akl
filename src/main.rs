a3ffcb60d79dbef2527c5b818bdbd48093473763
Héctor Salinas <hector_sag@outlook.com>
2024-07-16 08:42:47 -0600

    Get CPU Temp

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,26 @@
+use std::{thread::sleep, time::Duration};
+
 use hidapi::HidApi;
+use psutil::sensors;
 
 const VENDOR_ID: u16 = 0x3633;
 const PRODUCT_ID: u16 = 0x0003;
+const INTERVAL: u64 = 2;
 
-fn get_bar_value(value: u8) -> u8 {
-    return value - 1;
+fn get_bar_value(value: f64) -> f64 {
+    return value - 1.0;
 }
 
-fn get_data(value: u8, mode: &str) -> Vec<u8> {
+fn get_data(value: f64, mode: &str) -> Vec<u8> {
     let mut base_data = vec![0; 64];
     let numbers: Vec<u8> = value
         .to_string() // Convert the integer to a string
         .chars() // Get an iterator over the characters of the string
-        .map(|c| c.to_digit(10).unwrap() as u8) // Convert each character to a digit
+        .map(|c| c as u8) // Convert each character to a digit
         .collect();
 
     base_data[0] = 16;
-    base_data[2] = get_bar_value(value);
+    base_data[2] = get_bar_value(value) as u8;
 
     match mode {
         "start" => base_data[1] = 170,
@@ -46,6 +50,32 @@ fn get_data(value: u8, mode: &str) -> Vec<u8> {
     return base_data;
 }
 
+fn get_cpu_temperature() -> f64 {
+    let temperatures = sensors::temperatures();
+    let mut cpu_temp = 0.0;
+    let cpu_labels = ["CPU", "Tctl"];
+
+    for temperature in temperatures {
+        match temperature {
+            Ok(sensor_temp) => {
+                if cpu_labels.contains(&sensor_temp.label().unwrap_or_default()) {
+                    cpu_temp = sensor_temp.current().celsius();
+                }
+            }
+            Err(_) => (),
+        }
+    }
+
+    println!("CPU Temp: {}", cpu_temp);
+
+    cpu_temp
+}
+
+fn get_temp() -> Vec<u8> {
+    let temp = get_cpu_temperature();
+    get_data(temp, "temp")
+}
+
 fn main() {
     println!("Printing all available hid devices:");
 
@@ -59,7 +89,13 @@ fn main() {
             );
 
             ak.set_blocking_mode(false).unwrap();
-            ak.write(&get_data(0, "start")).unwrap();
+            ak.write(&get_data(0.0, "start")).unwrap();
+
+            loop {
+                ak.set_blocking_mode(false).unwrap();
+                ak.write(&get_temp()).unwrap();
+                sleep(Duration::from_secs(INTERVAL));
+            }
         }
         Err(e) => {
             eprintln!("Error: {}", e);
